ed517056db2a284df19986cc8c53f42bf2e88006
satya-hash <sathyagangadhara@gmail.com>
2024-08-21 19:01:51 +0530

    fix the search file

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,31 +1,31 @@
-"use client";
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { useDebounce } from "use-debounce";
+// "use client";
+// import { usePathname, useSearchParams, useRouter } from "next/navigation";
+// import React, { useEffect, useState } from "react";
+// import { useDebounce } from "use-debounce";
 
 const Search = () => {
-  const searchParams = useSearchParams();
-  const [text, setText] = useState("");
+  //   const searchParams = useSearchParams();
+  //   const [text, setText] = useState("");
 
-  const pathname = usePathname();
-  const { replace } = useRouter();
-  const [query] = useDebounce(text, 750);
+  //   const pathname = usePathname();
+  //   const { replace } = useRouter();
+  //   const [query] = useDebounce(text, 750);
 
-  useEffect(() => {
-    console.log(query);
+  //   useEffect(() => {
+  //     console.log(query);
 
-    const params = new URLSearchParams(searchParams || undefined);
-    if (query) {
-      params.set("query", query);
-    } else {
-      params.delete("query");
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }, [query]);
+  //     const params = new URLSearchParams(searchParams || undefined);
+  //     if (query) {
+  //       params.set("query", query);
+  //     } else {
+  //       params.delete("query");
+  //     }
+  //     replace(`${pathname}?${params.toString()}`);
+  //   }, [query]);
   return (
     <input
-      onChange={(e) => setText(e.target.value)}
-      defaultValue={searchParams?.get("query")?.toString()}
+      //   onChange={(e) => setText(e.target.value)}
+      //   defaultValue={searchParams?.get("query")?.toString()}
       type="text"
       id="search-navbar"
       className="block w-full p-2 ps-10 text-sm outline-none border-none  bg-bg-code  rounded-lg"
